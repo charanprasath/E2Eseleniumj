384ccef89f59fce9292b778ed9ccb4322b7bfeac
chara <chara@DESKTOP-JBH4NDR>
2023-04-18 10:04:35 +0530

    automated git

---
+++ E2Eselenium_jenkinsint/src/Tc_sel/Tc_0012.java
@@ -0,0 +1,17 @@
+package Tc_sel;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Tc_0012 {
+	@Test
+	public void Tc_001(){
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello1");
+		
+	}
+
+}
